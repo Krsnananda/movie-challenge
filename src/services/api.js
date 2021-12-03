0435625221984b994c596dfc016af427c8e06769
Krsnananda <krsnananda@visie.com.br>
2021-12-03 16:17:16 -0300

    Adiciona services e endpoints

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const baseUrl = 'https://challenge-fleye.herokuapp.com'
+
+export const omdbapi = 'https://www.omdbapi.com/?i=tt3896198&apikey=434e709d&s='
+
+export const db = {
+  movies = baseUrl + '/movies',
+  favorites = baseUrl + '/favorites'
+}
